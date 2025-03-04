d31b366c6d37b4b15593435d67fbf32c72a5f5dd
wulfdewolf <wolf.de.wulf@ed.ac.uk>
2025-03-04 16:48:28 +0000

    file commands

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,15 +1,38 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			--{"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-		config = function()
-			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-		end,
-	},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      --{"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          use_libuv_file_watcher = true,
+          window = {
+            mappings = {
+              ["o"] = "system_open",
+              ["p"] = "system_folder_open",
+            },
+          },
+        },
+        commands = {
+          system_open = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+          end,
+          system_folder_open = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.jobstart({ "nautilus", path }, { detach = true })
+          end,
+        },
+      })
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+    end,
+  },
 }
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,8 +1,7 @@
 return {
   "lervag/vimtex",
   lazy = false,
-  tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    vim.g.vimtex_view_method = "zathura"
+  config = function()
+    vim.g.vimtex_compiler_remove_aux_files = 1
   end
 }
