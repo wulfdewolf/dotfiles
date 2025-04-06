2708f9d68fcb1da5f13bef9f901fb25b17a9bd9a
wulfdewolf <wolf.de.wulf@ed.ac.uk>
2025-04-06 20:03:02 +0100

    add sync script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Install nvim and tmux
-sudo apt update
-sudo apt upgrade -y
-sudo apt install neovim tmux
+# Install nvim
+sudo apt install neovim
 
 # Move all config to the right place
 mkdir ~/.config/nvim
 cp -r lua/ init.lua ~/.config/nvim
-cp .tmux.conf ~/.tmux.conf
 
 # Download and install nerdfont
 wget -qO nerd-font.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.zip" && unzip -j nerd-font.zip "0xProtoNerdFont-Regular.ttf" -d ~/.local/share/fonts && rm nerd-font.zip
