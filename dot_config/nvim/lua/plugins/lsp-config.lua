f7cf043fa9aae67fdbfc3dce0eba1957bba52817
wulfdewolf <wolf2wulf@gmail.com>
2025-10-09 09:35:15 +0000

    remove lsps

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -11,10 +11,7 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "basedpyright",
                     "html",
-                    "matlab_ls",
-                    "marksman",
                     "tinymist",
                     "ruff"
                 },
@@ -29,19 +26,7 @@ return {
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
-            lspconfig.basedpyright.setup({
-                capabilities = capabilities,
-                settings = {
-                    basedpyright = {
-                        analysis = {
-                            typeCheckingMode = "basic",
-                        },
-                    },
-                }
-            })
-            lspconfig.marksman.setup({})
             lspconfig.ruff.setup({})
-            lspconfig.matlab_ls.setup({})
             lspconfig.html.setup({
                 filetypes = { "html", "liquid" }
             })
@@ -53,20 +38,6 @@ return {
                     formatterPrintWidth = 88
                 }
             }
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-                callback = function(args)
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client == nil then
-                        return
-                    end
-                    if client.name == 'ruff' then
-                        -- Disable hover in favor of Pyright
-                        client.server_capabilities.hoverProvider = false
-                    end
-                end,
-                desc = 'LSP: Disable hover capability from Ruff',
-            })
             vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
         end,
     },
