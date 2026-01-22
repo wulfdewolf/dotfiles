598a96b9aece2c869502622f2a029b0ce98e2c49
wulfdewolf <wolf2wulf@gmail.com>
2026-01-22 22:30:24 +0000

    harpoon

---
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,21 @@
+vim.pack.add({
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+    {
+    src = "https://github.com/ThePrimeagen/harpoon.git",
+    version = "harpoon2",
+  },
+})
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "=", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
