7759e3e069aef4004e3b9b249c8210e71b5e72db
wulfdewolf <wolf2wulf@gmail.com>
2026-02-20 18:53:38 +0000

    cmp keybindings

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -10,10 +10,10 @@ cmp.setup({
         completeopt = "menu,menuone,noinsert",
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<tab>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = {
         { name = "nvim_lsp" },
