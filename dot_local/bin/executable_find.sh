5aab8373d70b11ee5237e634b42972ab83fa38e6
wulfdewolf <wolf2wulf@gmail.com>
2026-05-06 19:15:58 +0100

    fixes

--- dot_local/bin/executable_find.sh
+++ dot_local/bin/executable_find.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-file=$(find ~ -type f 2>/dev/null | fzf)
-[ -n "$file" ] && nohup xdg-open "$(dirname "$file")" >/dev/null 2>&1 &
+set -e
+
+FILE=$(
+  fdfind -I -t f \
+    -E ~/.cache \
+    -E ~/.local/share/Trash \
+    -E ~/.local/share/Steam \
+    . ~ \
+  | fzf \
+      --prompt="Find file > " \
+      --preview '
+        batcat --style=numbers --color=always {} 2>/dev/null ||
+        (file {} | cut -d: -f2- && ls -lh {})
+      ' \
+      --preview-window=right:50%
+)
+
+[ -n "$FILE" ] && xdg-open "$FILE"
