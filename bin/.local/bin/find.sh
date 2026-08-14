35468fb2bc91e0eed67bd481f88c3416eeeeccea
wulfdewolf <wolf2wulf@gmail.com>
2026-08-14 18:13:07 +0100

    bin files

---
+++ bin/.local/bin/find.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+FILE=$(
+  fd -I -t f \
+    -E ~/.cache \
+    -E ~/.local/share/Trash \
+    -E ~/.local/share/Steam \
+    . ~ \
+  | fzf \
+      --prompt="Find file > " \
+      --preview '
+        batcat --style=numbers --color=always {} 2>/dev/null ||
+        (file {} | cut -d: -f2- && ls -lh {})
+      ' \
+      --preview-window=right:50%
+)
+
+[ -n "$FILE" ] && xdg-open "$FILE"
